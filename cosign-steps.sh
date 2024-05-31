0c1cced03fff239552a9ed52450c5252c97141ad
piccadilly circus <baize.aspect0t@icloud.com>
2024-05-31 13:23:24 +0500

    test

--- cosign-steps.sh
+++ cosign-steps.sh
@@ -1,7 +1,7 @@
 export COSIGN_PASSWORD="testpassword"
 
 # Sign the docker image
-cosign sign --key cosign.key travisarchitectenterprise/python-hello-world
+yes |cosign sign --key cosign.key travisarchitectenterprise/python-hello-world
 
 # Verify the docker image
 cosign verify --key cosign.pub travisarchitectenterprise/python-hello-world
