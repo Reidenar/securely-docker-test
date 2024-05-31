e55d7b9c3b878d22d6e5a8cf37be55a34c626a01
Reidenar <145158773+Reidenar@users.noreply.github.com>
2024-05-31 12:47:12 +0200

    Update docker-steps.sh

--- docker-steps.sh
+++ docker-steps.sh
@@ -5,7 +5,7 @@ docker login -u "${DOCKER_HUB_USER}" -p "${DOCKER_HUB_PASSWORD}"
 docker build -t hello-world-python .
 
 # tag docker image
-docker tag hello-world-python arthanaharidas/python-hello-world
+docker tag hello-world-python reidenar/python-hello-world
 
 # Push the docker image
-docker push arthanaharidas/python-hello-world
+docker push reidenar/python-hello-world
