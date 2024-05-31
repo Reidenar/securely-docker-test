309167077ca1b61f2af87e4d094ea64b873038ce
Reidenar <145158773+Reidenar@users.noreply.github.com>
2024-05-31 12:46:26 +0200

    Update cosign-steps.sh

--- cosign-steps.sh
+++ cosign-steps.sh
@@ -1,10 +1,10 @@
 export COSIGN_PASSWORD="dockertest1"
 
 # Sign the docker image
-cosign sign --key cosign.key arthanaharidas/python-hello-world
+cosign sign --key cosign.key reidenar/python-hello-world
 
 # Verify the docker image
-cosign verify --key cosign.pub arthanaharidas/python-hello-world
+cosign verify --key cosign.pub reidenar/python-hello-world
 
 # Logout docker
 docker logout
